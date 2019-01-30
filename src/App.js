681bfbd141948e9ff86696e2780380137d4eb0b6
realiceman <youss.hark@gmail.com>
2019-01-30 17:44:39 -0500

    started to implement features

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React !!
+            mplementing react on branch feature
           </a>
         </header>
       </div>
